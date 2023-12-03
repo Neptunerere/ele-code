177cf021bec5192e2513e4c46cd036edab49446d
Neptune <neomunjeong@gmail.com>
2023-12-03 23:55:34 +0900

    init: 기본 세팅

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<div class="text-gray-800 font-semibold">
+    <span class="text-[#484C89] text-xl">&lt;ELE&gt;</span> Code
+</div>
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,5 @@
+<a href="/">
+    <div class="text-gray-800 font-semibold">
+        <span class="text-[#484C89] text-3xl">&lt;ELE&gt;</span> Code
+    </div>
+</a>
